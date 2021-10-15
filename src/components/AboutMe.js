8bacd87f7955f7eeda8c232caba859299b204d1b
Andy <apc518@ymail.com>
2021-10-14 22:58:47 -0700

    minor tweak: font size

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,19 +13,19 @@ export default function AboutMe(){
                 <br/><br/>
             </Hidden>
 
-            <Typography variant="h4" gutterBottom>
+            <Typography variant="h3" gutterBottom>
                 Hi, I'm Andy!
             </Typography>
 
-            <Typography variant="h6" style={{lineHeight: 1.2}} gutterBottom>
+            <Typography variant="h5" style={{lineHeight: 1.2}} gutterBottom>
                 I enjoy music and programming. I've run a <a href={youtubeLink} target="_blank" rel="noreferrer">YouTube channel</a> since 2016 that features a lot of music theory and more <a href={realisrVideo} target="_blank" rel="noreferrer">programming</a> than you might expect!<br/>
             </Typography>
 
-            <Typography variant="h6" style={{lineHeight: 1.2}} gutterBottom>
+            <Typography variant="h5" style={{lineHeight: 1.2}} gutterBottom>
                 In fact, I first got into programming to solve a music theory problem! I've since fallen in love with coding and computer science, and am currently in my senior year at the University of Puget Sound where I major in computer science.<br/>
             </Typography>
 
-            <Typography variant="h6" style={{lineHeight: 1.2}}>
+            <Typography variant="h5" style={{lineHeight: 1.2}}>
                 <a href={githubLink} target="_blank" rel="noreferrer">Github</a>
             </Typography>
         </Container>
