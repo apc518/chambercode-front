a1278d0bc8f47e2d81efed6975d029474934fe4f
Andy <apc518@ymail.com>
2021-10-16 04:43:18 -0700

    added support for environment vars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+import dotenv from 'dotenv';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
+
+try{
+    const dotenvResult = dotenv.config();
+    if(dotenvResult.error){
+        console.log(dotenvResult.error)
+    }
+}
+catch(e){
+    if(process.env.NODE_ENV !== "production") {
+        console.error(e);
+    }
+}
 
 ReactDOM.render(
   <React.StrictMode>
