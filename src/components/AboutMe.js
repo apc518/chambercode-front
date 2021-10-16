4915bd83811767f1d689ea643f711ef5bfc2d762
Andy <apc518@ymail.com>
2021-10-16 03:39:49 -0700

    added programming language section and other changes to about me page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,33 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {Container, Typography, Hidden} from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+import TechProjects from './TechProjects';
 
 const youtubeLink = "https://www.youtube.com/andychamberlainmusic";
 const realisrVideo = "https://www.youtube.com/watch?v=2b8rnIcBCTc";
 const githubLink = "https://github.com/apc518";
 
+const projects = [
+    {
+        id: 0,
+        tech: "Python",
+        techThumbnailSrc: "/assets/images/python-icon.svg",
+        title: "Mass Aliaser",
+        description: "A python script with a tkinter GUI that allows you to very efficiently speed up a folder of wav files without any anti-aliasing.",
+        href: "https://github.com/apc518/mass-aliasing/"
+    },
+    {
+        id: 1,
+        tech: "C#",
+        techThumbnailSrc: "/assets/images/c-sharp-icon.svg",
+        title: "Chess Engine",
+        description: "A C# chess engine. I used a library for move generation and the rules of the game, but I coded the negamax algorithm, alpha-beta pruning, and multi-threading.",
+        href: null // coming soon
+    },
+    {
+        id: 2,
+        tech: "JavaScript",
+        techThumbnailSrc: "/assets/images/javascript-icon.svg",
+        title: "ChamberCode",
+        description: "This site! And many embedded apps such as Flippr, Spookr, and Context Collapse.",
+        href: "https://github.com/apc518/chambercode-front/"
+    }
+]
+
 export default function AboutMe(){
+    const [subs, setSubs] = useState("0");
+
+    useEffect(() => {
+        fetch("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCgd3Z7ShK-HME-gCxC-OaTQ&key=AIzaSyASo5KIEQZh4XWX3oV4SVqSLKCdAhUmc2s")
+            .then((res) => {
+                res.json().then(data => {
+                    setSubs(data.items[0].statistics.subscriberCount)
+                })
+            })
+            .catch(e => console.error(e));
+    }, [])
+
+
     return (
-        <Container style={{paddingTop: 20}}>
-            <Hidden smUp implementation="css">
+        <Container style={{paddingTop: 20, display: "flex", flexDirection: "column", minHeight: "100vh"}}>
+            <Hidden mdUp implementation="css">
                 <br/><br/>
             </Hidden>
 
-            <Typography variant="h3" gutterBottom>
-                Hi, I'm Andy!
+            <Typography variant="h4" gutterBottom>
+                Hi, I'm Andy! 🎹 🖥️
             </Typography>
 
             <Typography variant="h5" style={{lineHeight: 1.2}} gutterBottom>
-                I enjoy music and programming. I've run a <a href={youtubeLink} target="_blank" rel="noreferrer">YouTube channel</a> since 2016 that features a lot of music theory and more <a href={realisrVideo} target="_blank" rel="noreferrer">programming</a> than you might expect!<br/>
+                I'm a programmer and musician. I have a <a href={youtubeLink} target="_blank" rel="noreferrer">YouTube channel</a> with about {subs} subscribers that features a lot of music theory and more <a href={realisrVideo} target="_blank" rel="noreferrer">programming</a> than you might expect!<br/>
             </Typography>
+
+            <Typography variant="h5" gutterBottom>Other hobbies of mine include basketball and philosophy 🏀 🤔</Typography>
 
             <Typography variant="h5" style={{lineHeight: 1.2}} gutterBottom>
-                In fact, I first got into programming to solve a music theory problem! I've since fallen in love with coding and computer science, and am currently in my senior year at the University of Puget Sound where I major in computer science.<br/>
+                My first ever program was to aid in experimenting with <a href="https://youtu.be/zZZySzJzMLQ" target="_blank" rel="noreferrer">negative harmony.</a> I've since fallen in love with coding and computer science, and am currently in my senior year at the University of Puget Sound where I major in computer science.<br/>
             </Typography>
 
-            <Typography variant="h5" style={{lineHeight: 1.2}}>
-                <a href={githubLink} target="_blank" rel="noreferrer">Github</a>
+            <Typography variant="h5" gutterBottom>
+                Below are several programming languages in which I am proficient. Click any of them to go to a project of mine that uses the language!
             </Typography>
+
+            <br/>
+            <TechProjects projects={projects}/>
+
+            <footer style={{marginTop: "auto", padding:"2em"}}>
+            <Typography align="center" variant="h5" style={{lineHeight: 1.2}}>
+                <a href={githubLink} target="_blank" rel="noreferrer">My Github</a> <GitHubIcon />
+            </Typography>
+            </footer>
         </Container>
     )
 }
---
+++ src/components/TechProjects.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import { makeStyles, Card, CardActionArea, CardContent, CardHeader, CardMedia, Grid } from '@material-ui/core';
+
+import Swal from 'sweetalert2';
+
+const useStyles = makeStyles({
+    logo: {
+        transition: '0.15s',
+        '&:hover': {
+            transform: 'scale(1.05)'
+        }
+    }
+});
+
+export default function TechProjects({projects}){
+    const classes = useStyles();
+
+    return (
+        <Grid container alignContent="center">
+            {projects.map(project => (
+                <Grid item key={project.id} xs={12} sm={6}>
+                    <Card 
+                        elevation={0}
+                        style={{
+                            background: project.id % 2 === 0 ? "#043" : "#034",
+                            color: "white",
+                            width: 300
+                        }}
+                    >
+                        <CardHeader title={project.title}>
+                            {project.tech}
+                        </CardHeader>
+                        <CardActionArea
+                            onClick={() => {
+                                if("href" in project && project.href){
+                                    window.open(project.href);
+                                }
+                                else{
+                                    Swal.fire({
+                                        title: "This link isn't available right now 😕 Try again later.",
+                                        toast: true,
+                                    })
+                                }
+                            }}
+                            className={classes.logo}
+                        >
+                            <CardMedia 
+                                style={{width: 150, height: 150, marginLeft: "25%"}}
+                                image={project.techThumbnailSrc}
+                            />
+                        </CardActionArea>
+                        <CardContent>
+                            {project.description}
+                        </CardContent>
+                    </Card>
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
