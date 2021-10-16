c10445d1338a79acbadb6b8269f1c1d82b8fab71
Andy <apc518@ymail.com>
2021-10-16 00:56:19 -0700

    put a responsive spacer at the top of pages that need it on mobile

--- src/components/MiniApp.js
+++ src/components/MiniApp.js
@@ -1,28 +1,38 @@
+import { Hidden } from '@material-ui/core';
 import React, { useEffect, useRef } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 
 import { miniAppStyles } from '../objects/styles';
+
+const appsThatNeedSpace = {
+    "matheroids": 0,
+    "latex-matrix": 1
+}
 
 export default function MiniApp({url}){
     const {appid} = useParams();
     const location = useLocation();
     const classes = miniAppStyles();
 
+    // focus the iframe on page load
     const miniApp = useRef(null);
-
     useEffect(() => {
         miniApp?.current?.focus();
     }, []);
     
 
     return (
+        <>
+        <Hidden smUp implementation="css">
+            {appid in appsThatNeedSpace ? <><br/><br/></> : <></>}
+        </Hidden>
         <iframe
             ref={miniApp}
             className={classes.miniAppFrame}
-            // width="100%"
             title={appid}
             sandbox="allow-modals allow-forms allow-top-navigation allow-same-origin allow-scripts allow-pointer-lock allow-orientation-lock allow-popups allow-presentation"
             src={appid ? `/static${location.pathname}` : url}
         />    
+        </>
     )
 }
