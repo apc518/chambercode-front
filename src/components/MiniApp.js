f94b5167e51b4a8d74c60c62c2a146fd75a920d7
apc518 <apc518@ymail.com>
2021-11-14 01:17:35 -0800

    added loading animation for MiniApp component

--- src/components/MiniApp.js
+++ src/components/MiniApp.js
@@ -1,6 +1,8 @@
 import { Hidden } from '@material-ui/core';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+import { CircularProgress } from '@material-ui/core';
 
 import { miniAppStyles } from '../objects/styles';
 import { apps } from '../objects/apps';
@@ -9,6 +11,7 @@ import NotFound from './NotFound';
 export default function MiniApp({ category, name }){
     const {appid} = useParams();
     const classes = miniAppStyles();
+    const [loading, setLoading] = useState(true);
 
     // focus the iframe on page load
     const miniApp = useRef(null);
@@ -18,27 +21,36 @@ export default function MiniApp({ category, name }){
 
     const app = apps[category].filter(a => a.name === appid ? appid : name)[0];
 
-    if(!app){
-        return (
-            <NotFound/>
-        )
+    if(!app) {
+        return <NotFound/>
     }
 
     return (
         <div style={{
             height: "100%",
             display: "flex",
-            flexDirection: "column"
+            flexDirection: "column",
         }}>
             <Hidden mdUp implementation="css">
                 {app.needsForeheadMdDown ? <><div style={{ minHeight: "50px" }}></div></> : <></>}
             </Hidden>
+            {loading ? <CircularProgress
+                style={{
+                    zIndex: -1,
+                    position: 'absolute',
+                    left: "50%",
+                    top: "30%",
+                    transform: "translate(200%, -50%)",
+                    animation: "none"
+                }}
+            /> : <></> }
             <iframe
                 ref={miniApp}
                 className={classes.miniAppFrame}
                 title={appid}
                 sandbox="allow-modals allow-forms allow-top-navigation allow-same-origin allow-scripts allow-pointer-lock allow-orientation-lock allow-popups allow-presentation"
                 src={app.url}
+                onLoad={() => setLoading(false)}
             />
         </div>
     )
